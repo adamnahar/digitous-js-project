d5967127e0850f4c1005764e288bc057122fab22
unknown <hassandahab11@gmail.com>
2021-01-06 17:25:45 +0100

    second version of project js

--- project.js
+++ project.js
@@ -1,47 +1,71 @@
-;
+
     
 
     var grid = [
-        ['T','C','F','R','K','F','C','T'],
-        ['P','P','P','P','P','P','P','P'],
-        [' ',' ',' ',' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ',' ',' ',' ',' '],
-        ['p','p','p','p','p','p','p','p'],
-        ['t','c','f','k','r','f','c','t'] ];
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    ];
       
       console.log(grid.join('\n') + '\n\n');
       grid[4][4] = grid[6][4];
       grid[6][4] = ' ';
       console.log(grid.join('\n'));
 
+    
+
+var x = grid.map(function /*component*/(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.speedX = 0;
+  this.speedY = 0;
+  this.x = x;
+  this.y = y;
+  this.update = function() {
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  this.newPos = function() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
 
 
-    this.advance = function()
-    {
-        var nextPosition = this.body[0].slice();
-        switch(this.direction){
-            case "left":
-                nextPosition[0] -= 1;
-                break;
-            case "right":
-                nextPosition[0] += 1;
-                break;
-            case "down":
-                nextPosition[1] += 1;
-                break;
-            case "up":
-                nextPosition[1] -= 1;
-                break;  
-            default:
-                throw("invalid direction");      
-                       
-        }
-        this.body.unshift(nextPosition);
-        this.body.pop();
+function updateGameArea() {
+  myGameArea.clear();
+  myGamePiece.newPos();
+  myGamePiece.update();
+}
+function moveup() {
+  myGamePiece.speedY -= 1;
+} 
 
-    };
+function movedown() {
+  myGamePiece.speedY += 1;
+}
+
+
+ function moveleft() {
+  myGamePiece.speedX -= 1;
+}
+
+
+ function moveright() {
+  myGamePiece.speedX += 1;
+}
+
+});
+console.log(x);
+
+
 
 
  function roverKeyDown() {
