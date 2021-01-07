62e8a6b2511a0a881bd759ff7babc0bdd3e89cd8
unknown <hassandahab11@gmail.com>
2021-01-07 16:03:02 +0100

    add functuon  moveForWard

--- project.js
+++ project.js
@@ -13,10 +13,19 @@
   ];
     
 
-var rover = {
-  direction: "N"
-}
+  /*
+  rover est un object qui contient des clés direction qui prendre la valeur N par défaut represént la direction vers le nord et W, E, S,
+  y represént l'horizontal et x represént le vertical.
 
+  */
+
+var rover = {
+  direction: "N",
+  x:0,
+  y:0
+};
+
+//turnRight c'est une function faire l'action de la direction a droite
 function turnRight(rover){
   if (rover.direction === "N"){
       rover.direction = "E";
@@ -24,15 +33,19 @@ function turnRight(rover){
       rover.direction = "S";
   }else if(rover.direction ==="S"){
       rover.direction ="W";
-  }else if(rover.direction ===" W"){
+  }else if(rover.direction ==="W"){
       rover.direction = "S";
   }
-  console.log(rover);
+  console.log("rover dans turnRight :", rover.direction);
 }
 turnRight(rover);
 turnRight(rover);
 turnRight(rover);
 turnRight(rover);
+
+
+//turnLeft c'est une function faire l'action de la direction a gauche.
+
 
 function turnLeft(rover){
   if (rover.direction === "N"){
@@ -41,12 +54,42 @@ function turnLeft(rover){
       rover.direction = "S";
   }else if(rover.direction ==="S"){
       rover.direction ="E";
-  }else if(rover.direction ===" E"){
+  }else if(rover.direction ==="E"){
       rover.direction = "N";
   }
-  console.log(rover);
+  console.log("rover dans turnLeft :",rover.direction);
 }
+
 turnLeft(rover);
 turnLeft(rover);
 turnLeft(rover);
 turnLeft(rover);
+console.log(rover.direction);
+
+
+//moveforWard c'est une function faire changemment de la position vertical et horizontal.
+
+function moveForWard(rover) {
+  // console.log(rover.direction);
+  if(rover.direction === "N"){
+      rover.y = rover.y+1
+  }
+ else if(rover.direction ==="E"){
+     rover.x = rover.x+1
+ }
+ else if (rover.direction ==="S"){
+     rover.y = rover.y+1
+ }
+ else if (rover.direction === "W"){
+     rover.x = rover.x+1
+ }
+ console.log(rover.x);
+ console.log(rover.y);
+
+}
+console.log(moveForWard());
+
+
+
+
+
