b275e2a83527ff9013cab05615666feff91e2a8f
unknown <hassandahab11@gmail.com>
2021-01-07 12:19:42 +0100

    add functions Lf & Rt

--- project.js
+++ project.js
@@ -1,93 +1,52 @@
 
-    
-
     var grid = [
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
-    ];
-      
-      console.log(grid.join('\n') + '\n\n');
-      grid[4][4] = grid[6][4];
-      grid[6][4] = ' ';
-      console.log(grid.join('\n'));
-
+      [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+  ];
     
 
-var x = grid.map(function /*component*/(width, height, color, x, y) {
-  this.width = width;
-  this.height = height;
-  this.speedX = 0;
-  this.speedY = 0;
-  this.x = x;
-  this.y = y;
-  this.update = function() {
-    ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+var rover = {
+  direction: "N"
+}
+
+function turnRight(rover){
+  if (rover.direction === "N"){
+      rover.direction = "E";
+  }else if(rover.direction ==="E"){
+      rover.direction = "S";
+  }else if(rover.direction ==="S"){
+      rover.direction ="W";
+  }else if(rover.direction ===" W"){
+      rover.direction = "S";
   }
-  this.newPos = function() {
-    this.x += this.speedX;
-    this.y += this.speedY;
+  console.log(rover);
+}
+turnRight(rover);
+turnRight(rover);
+turnRight(rover);
+turnRight(rover);
+
+function turnLeft(rover){
+  if (rover.direction === "N"){
+      rover.direction = "W";
+  }else if(rover.direction ==="W"){
+      rover.direction = "S";
+  }else if(rover.direction ==="S"){
+      rover.direction ="E";
+  }else if(rover.direction ===" E"){
+      rover.direction = "N";
   }
-
-
-function updateGameArea() {
-  myGameArea.clear();
-  myGamePiece.newPos();
-  myGamePiece.update();
+  console.log(rover);
 }
-function moveup() {
-  myGamePiece.speedY -= 1;
-} 
-
-function movedown() {
-  myGamePiece.speedY += 1;
-}
-
-
- function moveleft() {
-  myGamePiece.speedX -= 1;
-}
-
-
- function moveright() {
-  myGamePiece.speedX += 1;
-}
-
-});
-console.log(x);
-
-
-
-
- function roverKeyDown() {
-
-        var newDirection;
-        switch(grid){
-            case 37:
-                newDirection = "N";
-                break;
-            case 38:
-                newDirection = "N";
-                break;
-            case 39:
-                newDirection = "E";
-                break;
-            case 40:
-                newDirection = "W";
-                break; 
-            default:
-                return;   
-
-                                  
-        } 
-    }
- 
+turnLeft(rover);
+turnLeft(rover);
+turnLeft(rover);
+turnLeft(rover);
